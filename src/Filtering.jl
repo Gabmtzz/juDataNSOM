6401d5afbf51a5eabf08e9db45d8c82707009863
Gabmtzz <gabmtzz27@gmail.com>
2022-11-11 09:39:59 -0600

    u

--- src/Filtering.jl
+++ src/Filtering.jl
@@ -11,6 +11,21 @@ function funRec(n,n0,nC)
     fW
 end
 
+function Bf(x,x0,xC,n)
+    arg = ((x-x0)^2)/(xC^2)
+    
+    if n ==0
+        B = exp(-arg)
+    else
+        sum1 = 0
+        for i ∈ 0:n
+            sum1 += (1/factorial(i))*arg^i
+        end
+        B = sum1*exp(-arg)
+    end
+    B
+end
+
 function GetFourierCoefs1(data)
     n = length(data)
 
@@ -20,8 +35,14 @@ function GetFourierCoefs1(data)
     PSD,dtaF
 end
 
-function GetFilteredData1(dataF,n0,nc)
-    indices = funRec(length(dataF),n0,nc)
+function GetFilteredData1(dataF,n0,nc,option,n=0)
+    if option == "Rec"
+        indices = funRec(length(dataF),n0,nc)
+    elseif option == "GH"
+        indices = Bf.(1:length(dataF),n0,nc,n)
+    else
+        indices = ones(length(dataF))
+    end
 
     fhtat = indices.*dataF
 
@@ -30,12 +51,12 @@ function GetFilteredData1(dataF,n0,nc)
     return filtrD, indices
 end
 
-function getFiltImag1(datIm,n0,nct)
+function getFiltImag1(datIm,n0,nct,option,n=0)
     datFiltr = zeros(size(datIm))
     for i in 1:size(datIm,1)
         dta = datIm[i,:]
         _,dataF = DataAnNSOM.GetFourierCoefs1(dta)
-        filtrD,_ = DataAnNSOM.GetFilteredData1(dataF,n0,nct)
+        filtrD,_ = DataAnNSOM.GetFilteredData1(dataF,n0,nct,option,n)
         datFiltr[i,:] = filtrD
     end
     datFiltr
